51ef8f6bced155759b72a3edb07c3ae9fa30adee
Robinson D. S. Santos <robinsondssantos@gmail.com>
2020-02-12 11:34:30 -0300

    send email with template

---
+++ src/config/mail.js
@@ -0,0 +1,14 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '38a0c79fd82f92',
+    pass: 'a087d8416c8631',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+// Using https://mailtrap.io/
